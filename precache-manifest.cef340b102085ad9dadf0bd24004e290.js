19daf0d056fefc38f3b634549c47e859f63672f1
priyank satasiya <priyanksatasiya81099@gmail.com>
2019-06-23 11:50:59 +0530

    Updates

---
+++ precache-manifest.cef340b102085ad9dadf0bd24004e290.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd1c56effb9fe43ee4fde85e73123af",
+    "url": "/MemeGen/index.html"
+  },
+  {
+    "revision": "1a3c1585c7904aaa820e",
+    "url": "/MemeGen/static/css/main.3946a383.chunk.css"
+  },
+  {
+    "revision": "3b6319925c708acc83f2",
+    "url": "/MemeGen/static/js/2.c1f7c5b5.chunk.js"
+  },
+  {
+    "revision": "1a3c1585c7904aaa820e",
+    "url": "/MemeGen/static/js/main.0e1b7ace.chunk.js"
+  },
+  {
+    "revision": "8ffd52f0eeb7194c0686",
+    "url": "/MemeGen/static/js/runtime~main.bde39dcf.js"
+  },
+  {
+    "revision": "be324d5b695be0894732e4e02e8fcc9f",
+    "url": "/MemeGen/static/media/face.be324d5b.png"
+  }
+]);
